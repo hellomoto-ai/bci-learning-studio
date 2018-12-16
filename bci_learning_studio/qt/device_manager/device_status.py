89cc1295d27e66c9b2301fbf74d11e286e8f90f9
moto <43505542+moto-hellomoto-ai@users.noreply.github.com>
2018-12-15 16:37:17 -0800

    Adding railed status to DeviceStatus (#20)

--- bci_learning_studio/qt/device_manager/device_status.py
+++ bci_learning_studio/qt/device_manager/device_status.py
@@ -4,6 +4,10 @@ from PyQt5 import QtWidgets
 from .device_status_ui import Ui_DeviceStatus
 
 
+_RAILED_THREASHOLD = int(pow(2, 23) - 1000)
+_NEAR_RAILED_THREASHOLD = int(pow(2, 23) * 0.9)
+
+
 class DeviceStatus(QtWidgets.QWidget):
     """Show board status"""
     def __init__(self, parent=None):
@@ -21,15 +25,27 @@ class DeviceStatus(QtWidgets.QWidget):
         """Initialize board information and sampling rate buffer"""
         self._set_sample_rate(0)
         self.ui.boardInfo.setText(board_info)
+        self.ui.measurementStatusIndicator.reset_status()
 
     def _set_sample_rate(self, sample_rate):
         self.ui.sampleRate.setText('%.2f' % sample_rate)
+
+    def _update_indicator(self, raw_eeg):
+        if any(abs(val) > _RAILED_THREASHOLD for val in raw_eeg):
+            self.ui.measurementStatusIndicator.set_railed()
+        elif any(abs(val) > _NEAR_RAILED_THREASHOLD for val in raw_eeg):
+            self.ui.measurementStatusIndicator.set_near_railed()
+        else:
+            self.ui.measurementStatusIndicator.set_ok()
+        self.ui.measurementStatusIndicator.update()
 
     def tick(self, sample):
         """Add sample timestamp to buffer and update sample rate label"""
         latest = sample['timestamp']
         self._buffer.append(latest)
         self._buffer = self._buffer[-1000:]
+
+        self._update_indicator(sample['raw_eeg'])
 
         if latest - self._last_update > 0.1 and len(self._buffer) > 10:
             sample_rate = (len(self._buffer) - 1) / (latest - self._buffer[0])
--- tests/device_manager/test_device_status.py
+++ tests/device_manager/test_device_status.py
@@ -1,4 +1,9 @@
-from bci_learning_studio.qt.device_manager import device_status
+from bci_learning_studio.qt.device_manager import (
+    device_status,
+    measurement_status_indicator,
+)
+
+# pylint: disable=protected-access
 
 
 def test_device_status(qtbot):
@@ -19,7 +24,47 @@ def test_device_status(qtbot):
     for sample_rate in [250, 500, 1000, 8000, 16000]:
         diff = 1 / sample_rate
         for _ in range(5000):
-            sample = {'timestamp': timestamp}
+            sample = {
+                'timestamp': timestamp,
+                'raw_eeg': [],
+            }
             status.tick(sample)
             timestamp += diff
         assert status.ui.sampleRate.text() == '%.2f' % sample_rate
+
+    # Test indicator
+    indicator = status.ui.measurementStatusIndicator
+    railed = int(pow(2, 23))
+    for val in [railed, railed - 999]:
+        raw_eeg = [0] * 16
+        for sign in [1, -1]:
+            raw_eeg[0] = sign * val
+            sample = {
+                'timestamp': 0,
+                'raw_eeg': raw_eeg,
+            }
+            status.tick(sample)
+            assert indicator._status == measurement_status_indicator._RAILED
+
+    for val in [railed-1001, int(railed * 0.9) + 1]:
+        raw_eeg = [0] * 16
+        for sign in [1, -1]:
+            raw_eeg[0] = sign * val
+            sample = {
+                'timestamp': 0,
+                'raw_eeg': raw_eeg,
+            }
+            status.tick(sample)
+            assert indicator._status == measurement_status_indicator._NEAR_RAILED
+
+    for val in [1, int(railed * 0.9) - 1]:
+        raw_eeg = [0] * 16
+        for sign in [1, -1]:
+            raw_eeg[0] = sign * val
+            sample = {
+                'timestamp': 0,
+                'raw_eeg': raw_eeg,
+            }
+            status.tick(sample)
+            assert indicator._status == measurement_status_indicator._OK
+
