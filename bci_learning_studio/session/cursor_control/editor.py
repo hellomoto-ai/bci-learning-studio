2bbb3c77712a711f554d1f450f7f1123d98d2ee0
moto <43505542+moto-hellomoto-ai@users.noreply.github.com>
2019-01-01 22:44:12 -0800

    Use Sample Viewer in cursor editor (#29)

--- bci_learning_studio/session/cursor_control/editor.py
+++ bci_learning_studio/session/cursor_control/editor.py
@@ -1,10 +1,10 @@
 import os
 
 import umsgpack
-import pyqtgraph
-from PyQt5 import QtCore, QtGui, QtWidgets
+import numpy as np
+from PyQt5 import QtCore, QtWidgets
 
-from bci_learning_studio.qt import qt_util, qg_util
+from bci_learning_studio.qt import qt_util
 from .editor_ui import Ui_Editor
 
 
@@ -22,28 +22,41 @@ def _ask_open_path(parent):
 
 
 def _load_file(path):
-    data = []
     with open(path, 'br') as fileobj:
         while True:
             try:
-                data.append(umsgpack.load(fileobj))
+                yield umsgpack.load(fileobj)
             except umsgpack.InsufficientDataException:
                 break
-    return data
 
 
-def get_context_menus(parent, n_channels):
-    group = QtWidgets.QFrame(parent)
-    group.setGeometry(QtCore.QRect(0, 0, 154, 79))
-    group.setObjectName('group')
-    layout = QtWidgets.QGridLayout(group)
-    layout.setObjectName('gridLayout')
-    for i in range(n_channels):
-        check = QtWidgets.QCheckBox(group)
-        check.setObjectName('plot%s' % i)
-        check.setText('Plot %s' % i)
-        layout.addWidget(check, i, 0, 1, 1)
-    return group
+def _parse_data(path):
+    eeg_data = {
+        'sample_rate': None,
+        'timestamps': [],
+        'samples': [],
+    }
+    cursor_data = []
+    target_data = {
+        'timestamps': [],
+        'coords': [],
+    }
+    for datum in _load_file(path):
+        if datum['type'] == 'eeg':
+            eeg_data['sample_rate'] = datum['data']['sampling_rate']
+            eeg_data['timestamps'].append(datum['data']['timestamp'])
+            eeg_data['samples'].append(datum['data']['eeg'])
+        elif datum['type'] == 'cursor':
+            cursor_data.append(datum['data'])
+        else:
+            target_data['timestamps'].append(datum['data']['time'])
+            target_data['coords'].append(
+                [datum['data']['x'], datum['data']['y']])
+    eeg_data['timestamps'] = np.asarray(eeg_data['timestamps'])
+    eeg_data['samples'] = np.asarray(eeg_data['samples']).T
+    target_data['timestamps'] = np.asarray(target_data['timestamps'])
+    target_data['coords'] = np.asarray(target_data['coords'])
+    return {'eeg': eeg_data, 'cursor': cursor_data, 'target': target_data}
 
 
 class Editor(QtWidgets.QMainWindow):
@@ -58,9 +71,7 @@ class Editor(QtWidgets.QMainWindow):
         qt_util.restore_window_position(self)
 
         self._filename = None
-        self._eeg_data = []
-        self._cursor_data = []
-        self._target_data = []
+        self._data = None
         self._bar = None
 
     ###########################################################################
@@ -91,74 +102,19 @@ class Editor(QtWidgets.QMainWindow):
             self._load_file(self._filename)
 
     def _load_file(self, path):
-        data = _load_file(path)
-        self._eeg_data = []
-        self._cursor_data = []
-        self._target_data = []
-        for datum in data:
-            if datum['type'] == 'eeg':
-                self._eeg_data.append(datum['data'])
-            elif datum['type'] == 'cursor':
-                self._cursor_data.append(datum['data'])
-            elif datum['type'] == 'target':
-                self._target_data.append(datum['data'])
+        self._data = _parse_data(path)
         self._plot()
 
     def _plot(self):
-        self.ui.graphWidget.clear()
-        plot = self.ui.graphWidget.addPlot(
-            row=1, col=1, title=os.path.split(self._filename)[-1],
-            axisItems={'bottom': qg_util.TimeAxisItem(orientation='bottom')},
-        )
-        plot.showGrid(x=True, y=True, alpha=1)
-        plot.setMouseEnabled(x=True, y=False)
-
-        x_min, x_max = float('inf'), 0.0
-        y_min, y_max = float('inf'), 0.0
-        for i in range(16):
-            x = [d['timestamp'] for d in self._eeg_data]
-            y = [d['eeg'][i] for d in self._eeg_data]
-            plot.plot(x, y, pen=pyqtgraph.mkPen(color=(0, 0, 255), width=3))
-            x_min = min(x_min, min(x))
-            x_max = max(x_max, max(x))
-            y_min = min(y_min, min(y))
-            y_max = max(y_max, max(y))
-        max_x_range = 1.1 * (x_max - x_min)
-        min_x = x_max - max_x_range
-        max_x = x_min + max_x_range
-        plot.getViewBox().setLimits(
-            maxXRange=max_x_range, xMin=min_x, xMax=max_x)
-        menu = plot.getMenu()
-        # TODO: Add plot reaction
-        grp = get_context_menus(menu, 16)
-        name = 'Data'
-        sm = QtGui.QMenu(name)
-        act = QtGui.QWidgetAction(plot)
-        act.setDefaultWidget(grp)
-        sm.addAction(act)
-        plot.subMenus.append(sm)
-        plot.ctrlMenu.addMenu(sm)
-
-        for datum in self._target_data:
-            plot.addLine(
-                x=datum['time'],
-                pen=pyqtgraph.mkPen(color=(0, 128, 0), width=3),
-                markers=[('o', 0.0, 10.0), ('o', 1.0, 10.0)],
-            )
-        self._bar = plot.addLine(
-            x=x_min, pen=pyqtgraph.mkPen(color=(0, 0, 0), width=3),
-            movable=True, bounds=[x_min, x_max],
-            markers=[('^', 0.0, 10.0), ('v', 1.0, 10.0)],
-        )
-        self._bar.sigDragged.connect(self._update_replay)
+        self.ui.viewer.plot(self._data['eeg'], self._data['target'])
+        self.ui.viewer.seek_bar_dragged.connect(self._update_replay)
 
         self.ui.cursorReplay.set_cursor(x=0, y=1)
         self.ui.cursorReplay.set_target(x=0, y=0.5)
         self.ui.cursorReplay.update()
 
     ##########################################################################
-    def _update_replay(self):
-        timestamp = self._bar.getXPos()
+    def _update_replay(self, timestamp):
         cursor = self._get_cursor_data(timestamp)
         if cursor:
             self.ui.cursorReplay.set_cursor(x=cursor['x'], y=cursor['y'])
@@ -167,8 +123,9 @@ class Editor(QtWidgets.QMainWindow):
             self.ui.cursorReplay.update()
 
     def _get_cursor_data(self, timestamp):
-        ret = self._cursor_data[0]
-        for datum in self._cursor_data:
+        ret = self._data['cursor'][0]
+        for datum in self._data['cursor']:
             if datum['time'] > timestamp:
-                return ret
+                break
             ret = datum
+        return ret
