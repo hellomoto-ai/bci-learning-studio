1bd860b73b7d78f013504a735de4bc3e738601a4
moto <moto@hellomoto.ai>
2019-01-30 22:46:25 -0800

    Adding seek bar

--- debug/test_vispy/plotter_widget.py
+++ debug/test_vispy/plotter_widget.py
@@ -19,22 +19,33 @@ def _get_lim(vals, margin=0.05):
 class _ViewBoxController:
     """Class to controll multiple viewboxes and lines as a group"""
     def __init__(self):
-        self.scale = 1.0
+        self._scale = 1.0
         self._lines = []
         self._viewboxes = []
+        self._seek_bars = []
+
         self._x_lim = None
         self._y_lim = None
 
-    def append(self, viewbox, line):
+    @property
+    def viewboxes(self):
+        return list(self._viewboxes)
+
+    @property
+    def seek_bars(self):
+        return list(self._seek_bars)
+
+    def append(self, viewbox, line, seek_bar):
         """Add ViewBox and Line object that are controlled by this class."""
         self._viewboxes.append(viewbox)
         self._lines.append(line)
+        self._seek_bars.append(seek_bar)
 
     def _adjust_scale(self, scale):
         """Adjust zoom scale so as not to zoom out too much."""
         scale = max(0.1, scale)
-        if self.scale * scale > 1.0:
-            scale = 1.0 / self.scale
+        if self._scale * scale > 1.0:
+            scale = 1.0 / self._scale
         return scale
 
     def zoom(self, scale):
@@ -42,13 +53,7 @@ class _ViewBoxController:
         scale = self._adjust_scale(scale)
         for vbox in self._viewboxes:
             vbox.camera.zoom(factor=[scale, 1], center=vbox.camera.center)
-        self.scale *= scale
-
-    def get_pan(self, event, previous_event):
-        """Get pan value between two mouse events."""
-        pos1, pos2 = event.pos, previous_event.pos
-        func = self._viewboxes[0].camera.transform.imap
-        return func(np.asarray(pos2[:2])) - func(np.asarray(pos1[:2]))
+        self._scale *= scale
 
     def _adjust_pan(self, pan):
         """Adjust pan value so as not to go beyond plot lines."""
@@ -84,10 +89,14 @@ class _ViewBoxController:
         """Reset view range"""
         for vbox in self._viewboxes:
             vbox.camera.set_range(x=self._x_lim, y=self._y_lim, margin=0)
-        self.scale = 1.0
+        self._scale = 1.0
+
+    def set_seek_bar(self, pos):
+        for seek_bar in self._seek_bars:
+            seek_bar.set_data(pos)
 
 
-def _add_plot_components(grid, max_axis_width, line_color):
+def _add_plot_components(grid, max_axis_width, line_color, add_seek_bar=True):
     # Add plot line, Y-Axis, and grid
     yaxis = vispy_util.get_axis(orientation='left', minor_tick_length=0)
     yaxis.width_max = max_axis_width
@@ -96,11 +105,15 @@ def _add_plot_components(grid, max_axis_width, line_color):
     line = vispy.scene.visuals.Line(np.array([[0, 0]]), color=line_color)
     viewbox.add(vispy.scene.visuals.GridLines())
     viewbox.add(line)
+    seek_bar = None
+    if add_seek_bar:
+        seek_bar = vispy.scene.visuals.InfiniteLine(pos=0, color=(1, 0, 0, 1))
+        viewbox.add(seek_bar)
 
     grid.add_widget(yaxis, row=0, col=0)
     grid.add_widget(viewbox, row=0, col=1)
     yaxis.link_view(viewbox)
-    return viewbox, line
+    return viewbox, line, seek_bar
 
 
 def _add_x_axis(grid, viewbox, max_axis_width):
@@ -115,21 +128,65 @@ def _add_x_axis(grid, viewbox, max_axis_width):
 
 
 def _create_plots(
-        n_plots, widget,
+        n_plots, widget, interactive,
         axis_width=30, line_color=(0.08, 0.08, 1.0, 0.8)):
     kwargs = {'margin': 0, 'spacing': 0, 'padding': 0}
     base_grid = widget.add_grid(**kwargs)
     ctrl = _ViewBoxController()
     for i in range(n_plots):
         grid = base_grid.add_grid(row=i, col=0, **kwargs)
-        viewbox, line = _add_plot_components(
-            grid, max_axis_width=axis_width, line_color=line_color)
-        ctrl.append(viewbox, line)
+        viewbox, line, seek_bar = _add_plot_components(
+            grid, max_axis_width=axis_width, line_color=line_color,
+            add_seek_bar=interactive,
+        )
+        ctrl.append(viewbox, line, seek_bar)
         if i == 0:
             grid = base_grid.add_grid(row=n_plots, col=0, **kwargs)
             grid.height_max = axis_width
             _add_x_axis(grid, viewbox, max_axis_width=axis_width)
     return ctrl
+
+
+class _MouseHandler:
+    def __init__(self, controller):
+        self._ctrl = controller
+
+        self._viewbox_clicked = None
+        self._seek_bar_drag = False
+
+    def process_event(self, event, picked):
+        if event.type == 'mouse_wheel':
+            self._ctrl.zoom(1 + event.delta[1])
+        elif event.type == 'mouse_press':
+            if picked in self._ctrl.viewboxes:
+                self._viewbox_clicked = picked
+                self._seek_bar_drag = self._is_seek_bar_clicked(event, picked)
+        elif event.type == 'mouse_move' and event.is_dragging:
+            if self._seek_bar_drag:
+                self._drag_seek_bar(event)
+            else:
+                self._pan(event, event.last_event)
+        elif event.type == 'mouse_release':
+            self._viewbox_clicked = None
+            self._seek_bar_drag = False
+
+    def _is_seek_bar_clicked(self, event, picked):
+        s_ev = vispy.scene.events.SceneMouseEvent(event=event, visual=picked)
+        seek_bar = self._ctrl.seek_bars[self._ctrl.viewboxes.index(picked)]
+        pos = picked.camera.transform.map(np.asarray([seek_bar.pos, 0, 0, 1]))
+        return abs(pos[0] - s_ev.pos[0]) < 3
+
+    def _drag_seek_bar(self, event):
+        scene_event = vispy.scene.events.SceneMouseEvent(
+            event=event, visual=self._viewbox_clicked)
+        coord = self._viewbox_clicked.camera.transform.imap(scene_event.pos)
+        self._ctrl.set_seek_bar(coord[0])
+
+    def _pan(self, event, previous_event):
+        pos1, pos2 = event.pos, previous_event.pos
+        func = self._ctrl.viewboxes[0].camera.transform.imap
+        pan = func(np.asarray(pos2[:2])) - func(np.asarray(pos1[:2]))
+        self._ctrl.pan(pan)
 
 
 class Plotter(vispy.scene.SceneCanvas):
@@ -140,7 +197,8 @@ class Plotter(vispy.scene.SceneCanvas):
         self.n_plots = n_plots
         self.interactive = interactive
 
-        self._controller = _create_plots(n_plots, self.central_widget)
+        self._controller = _create_plots(n_plots, self.central_widget, interactive)
+        self._mouse_handler = _MouseHandler(self._controller)
         self.central_widget.padding = 15
 
     def reset_range(self):
@@ -151,11 +209,12 @@ class Plotter(vispy.scene.SceneCanvas):
 
     def _process_mouse_event(self, event):
         if self.interactive:
-            if event.type == 'mouse_wheel':
-                self._controller.zoom(1 + event.delta[1])
-            elif event.type == 'mouse_move' and event.is_dragging:
-                pan = self._controller.get_pan(event, event.last_event)
-                self._controller.pan(pan)
+            picked = None
+            if event.type == 'mouse_press':
+                # visual_at is an expensive operation
+                # so only performs when necessary.
+                picked = self.visual_at(event.pos)
+            self._mouse_handler.process_event(event, picked)
         event.handled = True
 
 
